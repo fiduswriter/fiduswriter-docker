deec00caaa0950f056c9162ba6ed8f12a01fddab
Johannes Wilm <mail@johanneswilm.org>
2025-08-04 15:23:24 +0200

    add missing comma, fixes #1

--- volumes/config/configuration.py
+++ volumes/config/configuration.py
@@ -71,7 +71,7 @@ INSTALLED_APPS = [
     "book",
     "citation_api_import",
     "pandoc",
-    # "pandoc_on_server"
+    # "pandoc_on_server",
     # "gitrepo_export",
     # "languagetool",
     # "ojs",
